18cac619e916c4d7d1976ecb4c126b56906a6a99
Jonathan Lemanski <jlemanski1@gmail.com>
2020-04-23 08:30:23 -0600

    Added form w/ netlify Forms for contacting me

--- materialPortfolioSite/src/pages/index.js
+++ materialPortfolioSite/src/pages/index.js
@@ -385,7 +385,29 @@ const IndexPage = () => (
       >
         <div className="w-100">
           <h2 className="mb-5">Contact Me</h2>
-          
+        
+          <form method="post" netlify-honeypot="bot-field" data-netlify="true">
+            <input type="hidden" name="bot-field" />
+            <label>
+              Name
+              <input type="text" name="name" id="name" />
+            </label>
+            <label>
+              Email
+              <input type="email" name="email" id="email" />
+            </label>
+            <label>
+              Subject
+              <input type="text" name="subject" id="subject" />
+            </label>
+            <label>
+              Message
+              <textarea name="message" id="message" rows="5" />
+            </label>
+            <button type="submit">Send</button>
+            <input type="reset" value="Clear" />
+          </form>
+      
         </div>
       </section>
       
