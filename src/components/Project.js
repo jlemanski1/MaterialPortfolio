0c61c5949f6ca787cf5fe4122355476987b401cc
Jonathan Lemanski <c_jonathanlemanski@costco.com>
2022-10-30 14:14:41 -0700

    Updated Project & section for exclusive butterCMS use

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,7 +6,7 @@ const Project = (props) => {
         <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
         <div className="resume-content">
           <h3 className="mb-0">{props.project_title}</h3>
-          <div className="subheading mb-3">{props.project_subtitle}</div>
+          {props.project_subtitle.length === 0 || <div className="subheading mb-3">{props.project_subtitle}</div>}
           <p>
             {props.project_description}
           </p>
@@ -15,7 +15,7 @@ const Project = (props) => {
           <span className="text-primary">{props.project_date_range}</span>
           <ul className="list-inline project-icons">
             <li className="list-inline-item">
-              {props.project_github_url.length ===0 || <a href={props.project_github_url} className="fab fa-github"/>}
+              {props.project_github_url.length === 0 || <a href={props.project_github_url} className="fab fa-github"/>}
               {props.project_live_url.length === 0 || <a href={props.project_live_url} className="fa-duotone fa-browser"/>}
               {props.project_playstore_url.length === 0 || <a href={props.project_playstore_url} className="fab fa-google-play"/>}
               {props.project_appstore_url.length === 0 || <a href={props.project_appstore_url} className="fab fa-app-store-ios"/>}
--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -9,7 +9,7 @@ const ProjectSection = (props) => {
             id={props.fields.scroll_anchor_id}>
             <div className="w-100">
             <h2 className="mb-5">{props.fields.section_title}</h2>
-            {props.fields.section_subtitle.length ===0 || <div className="subheading mb-3">{props.fields.section_subtitle}</div>}
+            {props.fields.section_subtitle.length === 0 || <div className="subheading mb-3">{props.fields.section_subtitle}</div>}
                 {props.fields.project_items.map(project => <Project key={project.project_title} {...project}/>)}
             </div>
         </section>
