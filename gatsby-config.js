04c7f6cd71ea43d4d3a84794f28506ce992f5656
Jonathan Lemanski <c_jonathanlemanski@costco.com>
2022-10-29 21:11:38 -0700

    New Gatsby project, integrated Butter CMS

---
+++ gatsby-config.js
@@ -0,0 +1,47 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const config = require('./config');
+
+module.exports = {
+  
+  pathPrefix: config.pathPrefix,
+  siteMetadata: {
+    title: config.siteTitle,
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: config.manifestName,
+        short_name: config.manifestShortName,
+        start_url: config.pathPrefix || config.manifestStartUrl,
+        background_color: config.manifestBackgroundColor,
+        theme_color: config.manifestThemeColor,
+        display: config.manifestDisplay,
+        icon: config.manifestIcon, // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-sass',
+    'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-source-buttercms`,
+      options: {
+        authToken: process.env.BUTTER_CMS_API_KEY,
+        // Optional. Returns values for the supplied content field keys.
+        contentFields: {
+          keys: [`faq_items`, `faq_headline`],
+          // Optional. Set to 1 to enable test mode for viewing draft content.
+          test: 0,
+        },
+        // Optional. Array of page slugs.
+        pages: [`homepage`, `blog`],
+        // Optional. Array of page types.
+        pageTypes: [`homepage`, `blog`],
+      },
+    },
+  ],
+  
+};
